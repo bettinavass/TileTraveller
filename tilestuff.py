679f4bffff704251d329b18061e8306d1075eeb1
bettinavass <vassbetka@gmail.com>
2018-09-18 21:17:20 +0000

    finished

--- tilestuff.py
+++ tilestuff.py
@@ -8,80 +8,89 @@ location = 1.1
 while location != 3.1:
     if location == 1.1:
         print("You can travel: (N)orth.")
-        direction = (input("Direction: "))  
+
+        direction = (input("Direction: ")) 
+        while direction.lower() not in ['n']:
+            print("Not a valid direction!")
+            direction = (input("Direction: ")) 
         if direction.lower() == "n":
             location = round(location + 0.1, 2)
-        elif direction.lower() != "n":
-            print("Not a valid direction!")
 
     elif location == 1.2:
          print("You can travel: (N)orth or (E)ast or (S)outh.")
          direction = (input("Direction: "))
+         while direction.lower() not in ['n', 's', 'e']:
+            print("Not a valid direction!")  
+            direction = (input("Direction: ")) 
          if direction.lower() == "n":
              location = round(location + 0.1, 2)
          elif direction.lower() == "s":
              location = round(location - 0.1, 2)
          elif direction.lower() == "e":
-             location = round(location + 1, 2)
-         else:
-            print("Not a valid direction!")    
+             location = round(location + 1, 2)  
 
     elif location == 1.3:
         print("You can travel: (E)ast or (S)outh.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['s', 'e']:
+            print("Not a valid direction!")  
+            direction = (input("Direction: ")) 
         if direction.lower() == "e":
             location = round(location + 1, 2)
         elif direction.lower() == "s":
             location = round(location - 0.1, 2)
-        else:
-            print("Not a valid direction!")
 
     elif location == 2.3:
         print("You can travel: (E)ast or (W)est.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['w', 'e']:
+            print("Not a valid direction!")
+            direction = (input("Direction: ")) 
         if direction.lower() == "w":
             location = round(location -1, 2)
         elif direction.lower() == "e":
             location = round(location +1, 2)
-        else:
-            print("Not a valid direction!")
 
     elif location == 2.2:
         print("You can travel: (S)outh or (W)est.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['s', 'w']:
+            print("Not a valid direction!")
+            direction = (input("Direction: ")) 
         if direction.lower() == "s":
             location = round(location -0.1, 2)
         elif direction.lower() == "w":
             location = round(location -1, 2)    
-        else:
-            print("Not a valid direction!") 
 
     elif location == 2.1:
         print("You can travel: (N)orth.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['n']:
+            print("Not a valid direction!")
+            direction = (input("Direction: ")) 
         if direction.lower() == "n":
-            location = round(location +0.1, 2)
-        else:
-            print("Not a valid direction!")  
+            location = round(location +0.1, 2)  
 
     elif location == 3.3:
         print("You can travel: (S)outh or (W)est.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['s', 'w']:
+            print("Not a valid direction!")
+            direction = (input("Direction: ")) 
         if direction.lower() == "w":
             location = round(location -1, 2)
         elif direction.lower() == "s":
-            location = round(location -0.1, 2)    
-        else:
-            print("Not a valid direction!")   
+            location = round(location -0.1, 2)      
 
     elif location == 3.2:
         print("You can travel: (N)orth or (S)outh.")
         direction = (input("Direction: "))
+        while direction.lower() not in ['n', 's']:
+            print("Not a valid direction!")
+            direction = (input("Direction: "))
         if direction.lower() == "n":
             location = round(location +0.1, 2)
         elif direction.lower() == "s":
-            location = round(location -0.1, 2)    
-        else:
-            print("Not a valid direction!")                                                                       
+            location = round(location -0.1, 2)                                                                           
 else:
     print("Victory!")
